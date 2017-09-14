7814bfe1da7e460e7617e8530ddde4ba4581d989
zhangjing <zhangjing@romenscd.cn>
2017-09-14 15:26:33 +0800

    WIP(全局): 增加配置文件的代码 使支持redux devTool工具

--- src/Redux/Store/Store.jsx
+++ src/Redux/Store/Store.jsx
@@ -1,12 +1,18 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, compose, combineReducers, applyMiddleware} from 'redux';
 import * as reducer from '../Reducer/Index';
 import thunk from 'redux-thunk';
+
+// redux devTool
+let composeEnhancers = compose;
+if (process.env.NODE_ENV === 'development') {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
 
 //创建一个 Redux store 来以存放应用中所有的 state，应用中应有且仅有一个 store。
 
 var store = createStore(
     combineReducers(reducer),
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk)),
 );
 
 export default store;
